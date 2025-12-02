a12622bc371c82e515c63b4acbdd9b24bb73f3d7
Jonathan Esguerra <78431919+Jon-18@users.noreply.github.com>
2025-12-02 13:51:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,17 +23,6 @@ dotenv.config();
 
 const app = express();
 
-const allowedOrigins = [
-  "https://clinicsibongaclinic.xyz",
-  "http://localhost:3000",
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
-
-
 app.use(cookieParser());
 app.use(express.json());
 
@@ -60,7 +49,7 @@ app.use("/api/getAllAppointmentsRoutes", getAllAppointments);
 app.use("/api/getAllPatient", getAllPatient);
 app.use("/uploadsReceipt", express.static("uploadsReceipt"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5432;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
